cfbd72a72a0b634180f3b3f510c0511e3732daf0
Gabriel Linhares <glinharesb@gmail.com>
2023-04-30 23:53:27 -0300

    fix: adjusts

--- buffer_reader.go
+++ buffer_reader.go
@@ -5,54 +5,56 @@ import (
 )
 
 type BufferReader struct {
-	buf    []byte
-	offset int
+	Buf    []byte
+	Offset int
 }
 
-func NewBufferReader(buffer []byte) *BufferReader {
-	return &BufferReader{buf: buffer}
+func NewBufferReader(Buffer []byte) *BufferReader {
+	return &BufferReader{Buf: Buffer}
 }
 
 func (r *BufferReader) Seek(pos int) {
-	if pos >= 0 && pos <= len(r.buf) {
-		r.offset = pos
+	if pos >= 0 && pos <= len(r.Buf) {
+		r.Offset = pos
 	}
 }
 
 func (r *BufferReader) Move(diff int) {
-	if r.offset+diff >= 0 && r.offset+diff <= len(r.buf) {
-		r.offset += diff
+	if r.Offset+diff >= 0 && r.Offset+diff <= len(r.Buf) {
+		r.Offset += diff
 	}
 }
 
 func (r *BufferReader) NextUInt32LE() uint32 {
-	if r.offset+4 <= len(r.buf) {
-		val := binary.LittleEndian.Uint32(r.buf[r.offset:])
-		r.offset += 4
+	if r.Offset+4 <= len(r.Buf) {
+		val := binary.LittleEndian.Uint32(r.Buf[r.Offset:])
+		r.Offset += 4
 		return val
 	}
+
 	return 0
 }
 
 func (r *BufferReader) NextUInt16LE() uint16 {
-	if r.offset+2 <= len(r.buf) {
-		val := binary.LittleEndian.Uint16(r.buf[r.offset:])
-		r.offset += 2
+	if r.Offset+2 <= len(r.Buf) {
+		val := binary.LittleEndian.Uint16(r.Buf[r.Offset:])
+		r.Offset += 2
 		return val
 	}
+
 	return 0
 }
 
 func (r *BufferReader) NextUInt8() uint8 {
-	if r.offset+1 > len(r.buf) {
+	if r.Offset+1 > len(r.Buf) {
 		return 0
 	}
 
-	val := uint8(r.buf[r.offset])
-	r.offset++
+	val := uint8(r.Buf[r.Offset])
+	r.Offset++
 	return val
 }
 
 func (r *BufferReader) Tell() int {
-	return r.offset
+	return r.Offset
 }
